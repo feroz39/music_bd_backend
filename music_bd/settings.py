da3c57f9e33617e26cfd5e9f548d3fb94021e89b
feroz39 <mdferozhossain39@gmail.com>
2021-02-22 13:44:31 +0600

    update setting.py to enable collectstatics

--- music_bd/settings.py
+++ music_bd/settings.py
@@ -125,10 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR.joinpath('static')
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
